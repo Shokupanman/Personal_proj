666d82bc15e4ee18be8732b6340e48f054309028
Shokupanman <Harrisonkaidehancock@gmail.com>
2019-11-18 11:59:49 -0700

    initial push

---
+++ src/Components/Dash/Dash.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Dash extends Component {
+  render() {
+    return (
+      <div>
+        <h1>DASH</h1>
+      </div>
+    )
+  }
+}
---
+++ src/Components/Schools/Schools.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Schools extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Schools</h1>
+      </div>
+    )
+  }
+}
