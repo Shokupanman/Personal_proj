04c01c6e7842738739b073382480e2e1f30847f2
Shokupanman <Harrisonkaidehancock@gmail.com>
2019-11-26 20:23:14 -0700

    lookin alright

--- src/Components/Schools/School.js
+++ src/Components/Schools/School.js
@@ -1,27 +1,27 @@
 import React from 'react'
-import '../Companies/Company.scss'
+import './School.scss'
 
 export default function Company(props) {
   console.log(props)
   const { name, city, in_state, out_of_state, img, cat } = props.school
   return (
     <div>
-      <section className="bigboi">
-        <div className="taskdeal">TASK DEAL</div>
-        <div className="headi">
-          <div className="catT">
-            <h1 className="title">{name}</h1>
-            <h3 className="text">Catogory: {cat}</h3>
+      <section className="bigboi2">
+        <section className="lander2">
+          <img src={img} alt="" className="pics2" />
+        </section>
+        <div className="headi2">
+          <div className="catT2">
+            <h1 className="title2">{name}</h1>
+            <h3 className="text2">Catogory: {cat}</h3>
           </div>
-          <section className="lander">
-            <img src={img} alt="" className="pics" />
-          </section>
+          <div className="taskdeal2">TASK DEAL</div>
         </div>
       </section>
-      <section className="description">
-        <h3 className="text">City: {city}</h3>
-        <h3 className="text"> Instate: {in_state}</h3>
-        <h3 className="text">Out of State: {out_of_state}</h3>
+      <section className="description2">
+        <h3 className="text2">City: {city}</h3>
+        <h3 className="text2"> Instate: {in_state}</h3>
+        <h3 className="text2">Out of State: {out_of_state}</h3>
       </section>
     </div>
   )
